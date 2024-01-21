bfcab89efe52dbb1c87e90b62cb9d786c51a9cf3
MariaBerbis <maria.berbis.m@gmail.com>
2024-01-21 14:55:01 +0000

    Added Bootstrap

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import "./Weather.css";
 import WeatherSearch from "./WeatherSearch";
 
@@ -15,65 +16,67 @@ export default function Weather() {
   };
   return (
     <div className="Weather">
-      <header>
-        <WeatherSearch />
-      </header>
-      <main className="main-container">
-        <div className="weather-app-data">
-          <div>
-            <h1 className="weather-app-city">{weatherData.city}</h1>
+      <div className="container">
+        <header>
+          <WeatherSearch />
+        </header>
+        <main className="main-container">
+          <div className="weather-app-data">
             <div>
-              <img src={weatherData.imgUrl} alt={weatherData.description} />
+              <h1 className="weather-app-city">{weatherData.city}</h1>
+              <div>
+                <img src={weatherData.imgUrl} alt={weatherData.description} />
+              </div>
+            </div>
+            <div className="weather-app-temperature-container">
+              <div className="weather-app-temperature">
+                {weatherData.temperature}
+              </div>
+              <div className="weather-app-unit">
+                <a href="/">°C</a> | <a href="/">°F</a>
+              </div>
             </div>
           </div>
-          <div className="weather-app-temperature-container">
-            <div className="weather-app-temperature">
-              {weatherData.temperature}
-            </div>
-            <div className="weather-app-unit">
-              <a href="/">°C</a> | <a href="/">°F</a>
-            </div>
-          </div>
-        </div>
-        <p className="weather-app-details">
-          <span>{weatherData.date},</span>{" "}
-          <span> {weatherData.description}</span>
-          <br />
-          Humidity: {weatherData.humidity}%<strong></strong>, Wind:{" "}
-          {weatherData.wind}km/h
-          <strong></strong>
-        </p>
-      </main>
-      <footer>
-        This project was coded by
-        <a
-          href="https://github.com/MariaBerbis"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Maria Berbis
-        </a>
-        , is
-        <a
-          href="https://github.com/MariaBerbis?tab=repositories"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>{" "}
-        and
-        <a
-          href="https://app.netlify.com/sites/my-weather-app-calima/overview"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          hosted on Netlify
-        </a>
-        .
-      </footer>
+          <p className="weather-app-details">
+            <span>{weatherData.date},</span>{" "}
+            <span> {weatherData.description}</span>
+            <br />
+            Humidity: {weatherData.humidity}%<strong></strong>, Wind:{" "}
+            {weatherData.wind}km/h
+            <strong></strong>
+          </p>
+        </main>
+        <footer>
+          This project was coded by
+          <a
+            href="https://github.com/MariaBerbis"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Maria Berbis
+          </a>
+          , is
+          <a
+            href="https://github.com/MariaBerbis?tab=repositories"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>{" "}
+          and
+          <a
+            href="https://app.netlify.com/sites/my-weather-app-calima/overview"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            hosted on Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
